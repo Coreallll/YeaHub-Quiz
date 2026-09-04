4cb038c5e4b1be870e6a4fa3b5a42d6f3d05114f
Coreallll <100600313+Coreallll@users.noreply.github.com>
2026-09-04 09:52:08 +0300

    delete legacy api

--- src/api/getCollectionIdData.ts
+++
@@ -1,8 +0,0 @@
-// import axios from "axios";
-// const BASE_URL = 'https://api.yeatwork.ru';
-//
-// export async function getCollectionById(id:number, signal?: AbortSignal) {
-//   const response = await axios.get(`${BASE_URL}/collections/${id}/public`, {signal});
-//
-//   return response.data;
-// }
